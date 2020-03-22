9624adcc29781c9f03151a1998903e86b6a05d5f
214 <214dimas@gmail.com>
2020-03-22 19:57:42 +0700

    clean up after moving exception handling to Main

--- src/main/java/com/aikam/testtask/search/SearchOperation.java
+++ src/main/java/com/aikam/testtask/search/SearchOperation.java
@@ -1,6 +1,5 @@
 package com.aikam.testtask.search;
 
-import com.aikam.testtask.JSONUtility;
 import com.aikam.testtask.TestTaskException;
 import org.json.JSONException;
 
@@ -9,10 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SearchOperation {
-    private static final String DBErrorMessage = "Произошла ошибка в базе данных";
-    private static final String JSONErrorMessage = "Неправильный синтаксис входного фала";
-
-
     public String execute(String input) throws TestTaskException, JSONException, SQLException {
         SearchJSONParser JSONParser = new SearchJSONParser();
         SearchDBController DBController = new SearchDBController();
